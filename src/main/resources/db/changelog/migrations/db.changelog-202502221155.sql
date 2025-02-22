88f091f1785e034b081479e0581a21b183c34a84
victorribeirog <vicrgon2010@hotmail.com>
2025-02-22 12:06:08 -0300

    Criando migrations

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202502221155.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset victor:202502221155
+--comment: blocks table create
+
+CREATE TABLE BLOCKS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    block_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    block_reason VARCHAR(255) NOT NULL,
+    unblock_at TIMESTAMP NULL,
+    unblock_reason VARCHAR(255) NOT NULL,
+    card_id BIGINT NOT NULL,
+    CONSTRAINT cards__blocks_fk FOREIGN KEY (card_id) REFERENCES CARDS(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE BLOCK
